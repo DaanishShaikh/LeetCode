1ac5b7502751b67279cf384829eca7a39c4730c9
DaanishShaikh <daanish222@gmail.com>
2024-03-04 23:10:23 +0530

    04-03-2024

--- bag_of_tokens.cpp
+++ bag_of_tokens.cpp
@@ -1,13 +1,45 @@
-class Solution {
-public:
-    int bagOfTokensScore(vector<int>& tokens, int power) {
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
         int score=0;
         int count=0;
-        for(int i=0 ; i<tokens.size();i++){
-            if(power>=tokens[i]){
-                count++;
-            }
+        int power;
+        cin>>power;
+        int n;
+        cin>>n;
+        vector<int>tokens(n);
+        for (auto i = 0; i < n; i++)
+        {
+            cin>>tokens[i];
         }
-        return count;
+        
+        sort(tokens.begin(),tokens.end());
+        for (auto i = 0; i < n; i++)
+        {
+            cout<<tokens[i]<<' ';
+        }
+        cout<<endl;
+        for(int i=0, j=tokens.size()-1 ; i<=j;){
+            cout<<"1 power="<<power<<"tokens["<<i<<"]="<<tokens[i]<<"scores="<<score<<"tokens["<<j<<"]="<<tokens[j]<<endl;
+
+            if(power>=tokens[i]){
+                score+=1;
+                power-=tokens[i];
+                                cout<<"2 power="<<power<<"tokens["<<i<<"]="<<tokens[i]<<"score="<<score<<endl;
+
+                i++;
+            }
+            if(tokens.size()==2){break;}
+            if(power<tokens[j]&&score==1){
+                score-=1;
+                power+=tokens[j];
+                                cout<<"3 power="<<power<<"tokens["<<j<<"]="<<tokens[j]<<"score="<<score<<endl;
+
+                j--;
+            }
+            if(i==j||i>j){break;}
+        }
+        cout<<score<<endl;
+        return 0;
     }
-};
+    
