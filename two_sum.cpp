5446dd3dd79261465a109d139bfd49da7f2c112d
DaanishShaikh <daanish222@gmail.com>
2024-04-20 08:37:14 +0530

    20-04-2024

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,16 +3,23 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int k,l;
-      for(int i=0 ; i<nums.size() ; i++){
-          for(int j=1 ; j<nums.size() ; j++){
-              if(nums[i]+nums[j]==target){
-                  k=i;
-                  l=j;
-              }
-          }
-      }
-      vector<int>p={k,l};
-        return p;  
+        vector<int>v;
+        bool flag=0;
+        for(int i=0 ; i<nums.size() ; i++){
+            for(int j=0 ; j<nums.size() ; j++){
+                if(nums[i]+nums[j]==target&&(i!=j)){
+                    v.push_back(i);
+                    v.push_back(j);
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+            return v;
+
     }
+    cout<<twosum<<endl;
 };
